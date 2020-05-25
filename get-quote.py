67b74f4bf2dfdb8779b4d5b37a56264f1830180a
kilchbergspleen <65652549+kilchbergspleen@users.noreply.github.com>
2020-05-25 11:27:27 +0200

    Update get-quote.py

--- get-quote.py
+++ get-quote.py
@@ -8,4 +8,12 @@ def primary():
   print(quotes[13])
 
 if __name__== "__main__":
-  main()
+  main("Approachable is better than simple.")
+  
+ f = open("quotes.txt")
+  #quotes = f.readlines()
+  #f.close()
+  
+  print(quotes[5])
+  
+  
